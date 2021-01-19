a3e48f03339459ca1dccdef3597ddf1f252a9b54
Ian Storms <IanStorms110@yahoo.com>
2021-01-19 15:12:25 -0500

    updated readme and script

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+console.log("hello world");
+
+function processResponse(response) {
+    return response.json();
+}
+
+// Show the joke on the page:
+// 1. Make the request using fetch()
+fetch("https://icanhazdadjoke.com/", {
+    headers: {
+        Accept: 'application/json' 
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    return data.joke
+})
+.then(theJoke => {
+    console.log(theJoke)
+})
+.catch(err => console.log(err))
+// 2. Grab the .joke
+// 3. "Render" it to the page
